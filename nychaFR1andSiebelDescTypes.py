4c27f73fde49b1fe2f04be79ff8b8e5d0c7d61e5
Louis Flores <louis.flores@progressqueens.com>
2016-10-08 16:05:33 -0400

    Add files via upload
    Lists used to conduct searches for lead, asbestos, mold, and mildew for NYCHA's first FOIL response and for the Siebel data of NYCHA's second FOIL response.

---
+++ nychaFR1andSiebelDescTypes.py
@@ -0,0 +1,63 @@
+# user/bin/env python3
+# -*- coding: utf-8 -*-
+# nychaFR1andSiebelDescTypes.py - Termes de recherche. 
+
+# NYCHA first FOIL response search terms for lead, asbestos, mold, and mildew.
+fr1LeadList      = ['Ceiling - Lead Paint',
+                    'Leak From Above - Needs Lead Testing',
+                    'Walls - Lead Paint']
+
+fr1AsbesList     = ['Leak From Above - Needs Asbestos Testing']
+
+fr1MoldList      = ['Ceiling - Mildew Mold', 'Walls - Mildew Mold']
+
+fr1MildewList    = ['Mildew - ', 'Mildew Condition - ',
+                    'Mildew Condition - Mildew',
+                    'Mildew Condition - Needs Cleaning',
+                    'Mildew Condition - Needs Painting',
+                    'Mildew Condition - Paint After Repair',
+                    'Mildew Condition - Vent OOO']
+
+# NYCHA second FOIL response search terms for lead, asbestos, mold, and mildew.
+siebelLeadList   = ['Bathtub Shower - Needs Lead Testing',
+                    'Ceiling - Lead Paint', 'Floor - Needs Lead Testing',
+                    'Heat - Needs Lead Testing', 'Lead - Ceramic Substrate with Lead',
+                    'Lead - Lead Apartment Abatement', 'Lead - Lead Apartment Testing',
+                    'Lead - Lead Ceiling Abatement', 'Lead - Lead Ceiling Testing',
+                    'Lead - Lead Closet Testing', 'Lead - Lead Door Testing',
+                    'Lead - Lead Floor Testing', 'Lead - Lead Pipe Abatement',
+                    'Lead - Lead Pipe Testing', 'Lead - Lead Wall Abatement',
+                    'Lead - Lead Wall Testing', 'Lead - Lead Window Testing',
+                    'Lead - Lead in Dust Clearance', 'Lead - Needs Abatement',
+                    'Lead - Needs Testing', 'Leak From Above - Needs Lead Testing',
+                    'Pipes - Needs Lead Testing', 'Sink - Needs Lead Testing',
+                    'Walls - Lead Paint', 'Walls - Needs Lead Testing']
+
+siebelAsbesList  = ['Asbestos - Asbestos Air Monitoring',
+                    'Asbestos - Asbestos Brick Abatement',
+                    'Asbestos - Asbestos Brick Testing',
+                    'Asbestos - Asbestos Ceiling Abatement',
+                    'Asbestos - Asbestos Ceiling Testing',
+                    'Asbestos - Asbestos Floor Abatement',
+                    'Asbestos - Asbestos Floor Testing',
+                    'Asbestos - Asbestos Pipe Abatement',
+                    'Asbestos - Asbestos Pipe Testing',
+                    'Asbestos - Asbestos Roof Abatement',
+                    'Asbestos - Asbestos Roof Testing',
+                    'Asbestos - Asbestos Wall Abatement',
+                    'Asbestos - Asbestos Wall Testing',
+                    'Asbestos - Needs Abatement', 'Asbestos - Needs Asbestos Testing',
+                    'Asbestos - Needs Testing', 'Bathtub Shower - Needs Asbestos Testing',
+                    'Ceiling - Needs Asbestos Testing',
+                    'Dryer Rack - Needs Asbestos Testing',
+                    'Floor - Needs Asbestos Testing', 'Heat - Needs Asbestos Testing',
+                    'Leak From Above - Needs Asbestos Testing',
+                    'Pipes - Needs Asbestos Testing', 'Sink - Needs Asbestos Testing',
+                    'Walls - Needs Asbestos Testing']
+
+siebelMoldList   = ['Ceiling - Mildew Mold', 'Walls - Mildew Mold']
+
+siebelMildewList = ['Mildew Condition - Mildew', 'Mildew Condition - Needs Cleaning',
+                    'Mildew Condition - Needs Painting',
+                    'Mildew Condition - Paint After Repair',
+                    'Mildew Condition - Vent Out of order']
